70c8f480f52eace9abbcf6e6389cb7c5f7af0db7
adrian <adrian.rojas.o@gmail.com>
2024-12-02 12:22:57 -0400

    componentes creados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './paginas/products/products.component';
+import { ProductDetailComponent } from './paginas/product-detail/product-detail.component';
+import { CreateProductComponent } from './paginas/create-product/create-product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'products', component: ProductsComponent},
+    {path: 'products_id', component:ProductDetailComponent},
+    {path: 'create', component:CreateProductComponent}
+];
